c4f4bcecbc022cc05c28461aa91570310f4cf219
azat-sengul <azat.sengul@hotmail.com>
2023-10-19 20:33:12 +0300

    Öğretmen için ekleme, silme, güncelleme işlemleri gerçekleştirildi.

---
+++ Controllers/OgretmenController.cs
@@ -0,0 +1,104 @@
+using efcoreApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace efcoreApp.Controllers
+{
+    public class OgretmenController : Controller
+    {
+
+        private readonly DataContext _context;
+
+        public OgretmenController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var ogretmen = await _context.Ogretmenler.ToListAsync();
+
+            return View(ogretmen);
+
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Ogretmen model)
+        {
+            _context.Ogretmenler.Add(model);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult>  Edit(int? id)
+        {
+
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var ogretmen = await _context.Ogretmenler.FindAsync(id);
+
+            return View(ogretmen);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Ogretmen model)
+        {
+
+            if(id != model.OgretmenId)
+            {
+                return NotFound();
+            }
+
+            _context.Ogretmenler.Update(model);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+            
+
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ogretmen = await _context.Ogretmenler.FindAsync(id);
+
+            return View(ogretmen);
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id, Ogretmen model)
+        {
+            if (id != model.OgretmenId)
+            {
+                return NotFound();
+            }
+
+            var ogretmen = await _context.Ogretmenler.FindAsync(id);
+
+            _context.Ogretmenler.Remove(ogretmen);
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+       
+
+        }
+
+
+
+
+    }
+}
